815d6ce62c75809ff220125c16bc610522e91453
Toni Suomalainen <toni.suomalainen@taitounited.fi>
2017-08-03 23:28:38 +0300

    added some boilerplate

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,1 +1,9 @@
-// Main js file
+import $ from 'jquery';
+
+// Hide Page Loader when DOM and images are ready
+$(window).on('load', () => $('.pageloader').removeClass('is-active'));
+
+// Toggle mobile menu
+$('.navbar-burger').on('click', () =>
+  $('.navbar-burger, .navbar-menu').toggleClass('is-active'));
+
