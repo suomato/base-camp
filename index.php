3a57d1814dcc19d62f306680aa9dc1cffdb0f9f1
Toni Suomalainen <toni.suomalainen@taitounited.fi>
2017-08-05 20:57:42 +0300

    removed main menu and sidebar from Timber context object

--- index.php
+++ index.php
@@ -2,7 +2,5 @@
 
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts(['posts_per_page' => -1]);
-$context['main_sidebar'] = Timber::get_widgets('main-sidebar');
-$context['menu'] = new TimberMenu('main_menu');
 
 Timber::render('index.twig', $context);
