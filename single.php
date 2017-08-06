81606f761fa539cf04e2c0004766f7009937ee93
Toni Suomalainen <toni.suomalainen@taitounited.fi>
2017-08-06 18:06:49 +0300

    updated Single post layout

--- single.php
+++ single.php
@@ -1,6 +1,11 @@
 <?php
 
-$context = Timber::get_context();
-$context['post'] = new TimberPost();
+$context         = Timber::get_context();
+$post            = new TimberPost();
+$context['post'] = $post;
 
-Timber::render('single.twig', $context);
+if (post_password_required($post->ID)) {
+    Timber::render('single-password.twig', $context);
+} else {
+    Timber::render(['single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig'], $context);
+}
