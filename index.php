4967dbd395ccc9cd13377892df6dda0dee77351c
Toni Suomalainen <toni.suomalainen@taitounited.fi>
2017-08-06 21:16:37 +0300

    refactor code

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$context = Timber::get_context();
+$context          = Timber::get_context();
 $context['posts'] = Timber::get_posts(['posts_per_page' => -1]);
 
 Timber::render('index.twig', $context);
