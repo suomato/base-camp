a505f75d7b06125b350347b1733ea01028c857d2
Toni Suomalainen <toni.suomalainen@taitounited.fi>
2017-08-07 20:19:49 +0300

    added author template

---
+++ author.php
@@ -0,0 +1,14 @@
+<?php
+
+global $wp_query;
+
+$context          = Timber::get_context();
+$context['posts'] = Timber::get_posts();
+
+// Check if author exists
+if (isset($wp_query->query_vars['author'])) {
+    $author            = new TimberUser($wp_query->query_vars['author']);
+    $context['author'] = $author;
+    $context['title']  = 'Author Archives: ' . $author->name();
+}
+Timber::render(array('author.twig', 'archive.twig'), $context);
