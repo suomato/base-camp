555e71b63aeed113bae29a65e25b607cce301cf7
Toni Suomalainen <toni.suomalainen@taitounited.fi>
2017-08-07 20:19:37 +0300

    added archive template

---
+++ archive.php
@@ -0,0 +1,25 @@
+<?php
+
+$templates        = ['archive.twig', 'index.twig'];
+$context          = Timber::get_context();
+$context['title'] = 'Archive';
+
+if (is_day()) {
+    $context['title'] = 'Archive: ' . get_the_date('D M Y');
+} else if (is_month()) {
+    $context['title'] = 'Archive: ' . get_the_date('M Y');
+} else if (is_year()) {
+    $context['title'] = 'Archive: ' . get_the_date('Y');
+} else if (is_tag()) {
+    $context['title'] = 'Tag: ' . single_tag_title('', false);
+} else if (is_category()) {
+    $context['title'] = 'Category: ' . single_cat_title('', false);
+    array_unshift($templates, 'archive-' . get_query_var('cat') . '.twig');
+} else if (is_post_type_archive()) {
+    $context['title'] = post_type_archive_title('', false);
+    array_unshift($templates, 'archive-' . get_post_type() . '.twig');
+}
+
+$context['posts'] = Timber::get_posts();
+
+Timber::render($templates, $context);
