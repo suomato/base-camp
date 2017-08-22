68481231c6a5e6879ebec921f7f9a91e164bcbd8
Toni Suomalainen <toni.suomalainen@taitounited.fi>
2017-08-22 18:34:55 +0300

    updated array

--- author.php
+++ author.php
@@ -11,4 +11,4 @@ if (isset($wp_query->query_vars['author'])) {
     $context['author'] = $author;
     $context['title']  = 'Author Archives: ' . $author->name();
 }
-Timber::render(array('author.twig', 'archive.twig'), $context);
+Timber::render(['author.twig', 'archive.twig'], $context);
