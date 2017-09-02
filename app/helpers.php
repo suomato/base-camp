e5b14030853ca993800e0b0b726a4e7e60ce169f
Toni Suomalainen <toni.suomalainen@taitounited.fi>
2017-09-02 22:48:02 +0300

    Separated Luna commands from theme to package

--- app/helpers.php
+++ app/helpers.php
@@ -26,16 +26,3 @@ function assets($key)
 
     return get_stylesheet_directory_uri() . "/static/" . $manifest_array[$key];
 }
-
-function build_custom_post_type($template, $name, $plural)
-{
-    $singular = $name;
-    $plural   = $plural ?? "{$name}s";
-
-    $content = str_replace('movies', $plural, $template);
-    $content = str_replace('Movies', ucfirst($plural), $content);
-    $content = str_replace('Movie', ucfirst($singular), $content);
-    $content = str_replace('movie', $singular, $content);
-
-    return $content;
-}
