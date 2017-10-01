6fd6dab09ed6bd9674c18dc2aeab805e901deb02
Toni Suomalainen <toni.suomalainen@taitounited.fi>
2017-10-01 13:43:25 +0300

    added comments

--- functions.php
+++ functions.php
@@ -3,6 +3,9 @@
 require_once(__DIR__ . '/vendor/autoload.php');
 require_once(__DIR__ . '/app/config/autoload.php');
 
+/**
+ * Loads the theme's translated strings.
+ */
 function localize()
 {
     load_theme_textdomain('base-camp', get_template_directory() . '/resources/languages');
